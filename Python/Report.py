546f4f79b292f3c4c308593f63d0207e3b0729c4
Chevelle-0 <117184276+Chevelle-0@users.noreply.github.com>
2022-11-18 16:11:42 +0000

    Fixed incorrect overseas code error message, improved output readability
    Wrapped overseas code requests in while loop to help with checking for errors, added newlines after input requests to improve output readability

--- Python/Report.py
+++ Python/Report.py
@@ -18,31 +18,40 @@ def printMenu():
   choice = 0
   while choice > 5 or choice < 1:
     choice = int(input("\nEnter a number from 1 to 5 representing your choice\n"))
+    print("\n")
   return choice
 
 def executeChoice(choice):
+  #VARIABLES
+  complete1 = 0
+  repeat = True
   #AIRPORT DETAILS
   if choice == 1:
     #UK CODE
     UKairportCode = input("Enter three letter code of UK airport\n")
+    print("\n")
     while UKairportCode != "LPL" and UKairportCode != "BOH":
       if UKairportCode != "LPL" and UKairportCode != "BOH":
-        print('Error: UK airport code must be "LPL" or "BOH"\n')
+        print('Error: UK airport code must be "LPL" or "BOH"!\n')
+      UKairportCode = input("Enter three letter code of UK airport\n")
+      print("\n")
     #OVERSEAS CODE
-    nonUKairportCode = input("Enter three letter code of overseas airport\n")
-    while nonUKairportCode == "LPL" and nonUKairportCode == "BOH":
+    while complete1 == 0:
       nonUKairportCode = input("Enter three letter code of overseas airport\n")
-      if nonUKairportCode == "LPL" and nonUKairportCode == "BOH":
-        print('Error: UK airport code must not be "LPL" or "BOH"\n')
-    for i in range(len(airportCodes)):
-      if nonUKairportCode == airportCodes[i]:
-        print("\nName of airport with given code:\n" + airportNames[i] + "\n")
-        complete1 = 1
-    if complete1 == 0:
-      print("Overseas airport code does not match any in database\n")
-  repeat = True
-  print("Operation complete, returning to main menu... \n")
-  return repeat
+      print("\n")
+      while nonUKairportCode == "LPL" and nonUKairportCode == "BOH":
+        nonUKairportCode = input("Enter three letter code of overseas airport\n")
+        while nonUKairportCode == "LPL" or nonUKairportCode == "BOH":
+          print('Error: overseas airport code must not be "LPL" or "BOH"\n')
+          nonUKairportCode = input("Enter three letter code of overseas airport\n") 
+      for i in range(len(airportCodes)):
+        if nonUKairportCode == airportCodes[i]:
+          print("\nName of airport with given code:\n" + airportNames[i] + "\n")
+          complete1 = 1
+      if complete1 == 0:
+        print("Error: Overseas airport code did not match any in database!\n")
+    print("Operation complete, returning to main menu... \n")
+    return repeat
   if choice == 5:
     print("Quitting...")
     quit()
