b873749f2cda9c06f3b6be2c1b961d6eea35f6f5
Chevelle-0 <117184276+Chevelle-0@users.noreply.github.com>
2022-12-02 14:02:49 +0000

    Tasks 5a, b, c, d finished
    need to add return statements to all said tasks

--- Python/Report.py
+++ Python/Report.py
@@ -1,7 +1,7 @@
 import time
 #LISTS
 airportCodes = ["JFK", "ORY", "MAD", "AMS", "CAI"]
-airportNames = ["John F Kennedy International", "Paris-Orly", "Adolfo Suarez MAdrid-Bajaras", "Amsterdam Schipol", "Cairo International"]
+airportNames = ["John F Kennedy International", "Paris-Orly", "Adolfo Suarez Madrid-Bajaras", "Amsterdam Schipol", "Cairo International"]
 distanceFromLPL = [5326, 629, 1428, 526, 3779]
 distanceFromBOH = [5486, 379, 1151, 489, 3584]
 details = [["Boeing 737", "£8", "2650", "180", "8"], ["Airbus 321", "£7", "5600", "220", "10"], ["Boeing 787", "£5", "4050", "406", "14"]]
@@ -14,6 +14,8 @@ complete1 = 0
 repeat = True
 firstClass = 0
 aircraftID = 0
+aircraftType = ""
+airportCodeNumber = 5
 
 #FUNCTIONS
 def printMenu():
@@ -24,129 +26,166 @@ def printMenu():
     print("\n")
   return choice
 
+#AIRPORT DETAILS
+def airportDetails():
+  complete = 0
+  complete1 = 0
+  #UK CODE
+  UKairportCode = input("Enter three letter code of UK airport\n")
+  print("\n")
+  while UKairportCode != "LPL" and UKairportCode != "BOH":
+    if UKairportCode != "LPL" and UKairportCode != "BOH":
+      print('Error: UK airport code must be "LPL" or "BOH"!\n')
+    UKairportCode = input("Enter three letter code of UK airport\n")
+    print("\n")
+  #OVERSEAS CODE
+  while complete1 == 0:
+    nonUKairportCode = input("Enter three letter code of overseas airport\n")
+    print("\n")
+    while nonUKairportCode == "LPL" and nonUKairportCode == "BOH":
+      nonUKairportCode = input("Enter three letter code of overseas airport\n")
+      while nonUKairportCode == "LPL" or nonUKairportCode == "BOH":
+        print('Error: overseas airport code must not be "LPL" or "BOH"\n')
+        nonUKairportCode = input("Enter three letter code of overseas airport\n") 
+    for i in range(len(airportCodes)):
+      if nonUKairportCode == airportCodes[i]:
+        print("\nName of airport with given code:\n" + airportNames[i] + "\n")
+        complete1 = 1
+    if complete1 == 0:
+      print("Error: Overseas airport code did not match any in database!\n")
+  time.sleep(2)
+  print("Operation complete, returning to main menu... \n")
+  time.sleep(1)
+  for i in range(5):
+    if airportCodes[i] == nonUKairportCode:
+      airportCodeNumber = i
+  return repeat
+
+def flightDetails():
+  aircraftType = input("Enter type of aircraft\n")
+  print("\n")
+  if aircraftType != details[0][0] and aircraftType != details[1][0] and aircraftType != details[2][0]:
+      print("Error: invalid aircraft type\n")
+      time.sleep(1)
+      print("Returning to main menu...\n")
+      time.sleep(1)
+      repeat = True
+      return repeat
+  elif aircraftType == details[0][0]:
+    aircraftID = 0
+    time.sleep(0.5)
+    print("Name: " + details[0][0])
+    time.sleep(0.5)
+    print("Running cost per seat per 100km: " + details[0][1])
+    time.sleep(0.5)
+    print("Maximum flight range (km): " + details[0][2])
+    time.sleep(0.5)
+    print("Maximum number of economy seats:: " + details[0][3])
+    time.sleep(0.5)
+    print("Maximum number of first-class seats: " + details[0][4])
+    print("\n")
+    time.sleep(1)
+    firstClass = int(input("Enter number of first class seats\n"))
+    if firstClass != 0:
+      if firstClass > int(details[aircraftID][4]):
+        print("\nError: number of first-class seats too large\n")
+        time.sleep(0.5)
+        print("Operation complete, returning to main menu...\n")
+        time.sleep(1)
+        repeat = True
+        return repeat
+      else:
+        time.sleep(0.5)
+        print(f"\nNumber of economy seats: {int(details[aircraftID][3]) - (firstClass*2)}")
+        time.sleep(1)
+    print("\nOperation complete, returning to main menu... \n")
+    time.sleep(1)
+    repeat = True
+    return repeat
+  elif aircraftType == details[1][0]:
+    aircraftID = 1
+    time.sleep(0.5)
+    print("Name: " + details[1][0])
+    time.sleep(0.5)
+    print("Running cost per seat per 100km: " + details[1][1])
+    time.sleep(0.5)
+    print("Maximum flight range (km): " + details[1][2])
+    time.sleep(0.5)
+    print("Maximum number of economy seats:" + details[1][3])
+    time.sleep(0.5)
+    print("Maximum number of first-class seats 5: " + details[1][4])
+    print("\n")
+    time.sleep(1)
+    firstClass = int(input("Enter number of first class seats\n"))
+    if firstClass != 0:
+      if firstClass > details[aircraftID][4]:
+        print("Error: number of first-class seats too large")
+        repeat = True
+        return repeat
+      else:
+        print(f"Number of economy seats: {details[aircraftID][3] - (firstClass*2)}")
+    print("\nOperation complete, returning to main menu... \n")
+    time.sleep(1)
+    repeat = True
+    return repeat
+  elif aircraftType == details[2][0]:
+    aircraftID = 2
+    time.sleep(0.5)
+    print("Name: " + details[2][0])
+    time.sleep(0.5)
+    print("Running cost per seat per 100km: " + details[2][1])
+    time.sleep(0.5)
+    print("Maximum flight range (km): " + details[2][2])
+    time.sleep(0.5)
+    print("Maximum number of economy seats: " + details[2][3])
+    time.sleep(0.5)
+    print("Maximum number of first-class seats: " + details[2][4])
+    print("\n")
+    time.sleep(1)
+    firstClass = int(input("Enter number of first class seats\n"))
+    if firstClass != 0:
+      if firstClass > details[aircraftID][4]:
+        print("Error: number of first-class seats too large")
+        repeat = True
+        return repeat
+      else:
+        print(f"Number of economy seats: {details[aircraftID][3] - (firstClass*2)}")
+    print("\nOperation complete, returning to main menu... \n")
+    time.sleep(1)
+    repeat = True
+    return repeat
+
+#ENTER PRICE PLAN
+def pricePlan():
+  if UKairportCode == "" or nonUKairportCode == "":
+    print("Error: codes for UK and non UK airports not entered")
+    repeat = True
+    return repeat
+  if aircraftType == "":
+    print("Error: aircraft type not entered")
+    repeat = True
+    return repeat
+  if firstClass == 0:
+    print("Error: number of first class seats not entered")
+  if UKaiportCode == "LPL" and distanceFromLPL[airportCodeNumber] > details[aircraftID][2]:
+    print("Error: maximum flight range less than distance between airports")
+  if UKaiportCode == "BOH" and distanceFromBOH[airportCodeNumber] > details[aircraftID][2]:
+    print("Error: maximum flight range less than distance between airports")
+  
+
 def executeChoice(choice):
   #VARIABLES
   complete1 = 0
   repeat = True
-  firstClass =0
+  firstClass = 0
   #AIRPORT DETAILS
   if choice == 1:
-    #UK CODE
-    UKairportCode = input("Enter three letter code of UK airport\n")
-    print("\n")
-    while UKairportCode != "LPL" and UKairportCode != "BOH":
-      if UKairportCode != "LPL" and UKairportCode != "BOH":
-        print('Error: UK airport code must be "LPL" or "BOH"!\n')
-      UKairportCode = input("Enter three letter code of UK airport\n")
-      print("\n")
-    #OVERSEAS CODE
-    while complete1 == 0:
-      nonUKairportCode = input("Enter three letter code of overseas airport\n")
-      print("\n")
-      while nonUKairportCode == "LPL" and nonUKairportCode == "BOH":
-        nonUKairportCode = input("Enter three letter code of overseas airport\n")
-        while nonUKairportCode == "LPL" or nonUKairportCode == "BOH":
-          print('Error: overseas airport code must not be "LPL" or "BOH"\n')
-          nonUKairportCode = input("Enter three letter code of overseas airport\n") 
-      for i in range(len(airportCodes)):
-        if nonUKairportCode == airportCodes[i]:
-          print("\nName of airport with given code:\n" + airportNames[i] + "\n")
-          complete1 = 1
-      if complete1 == 0:
-        print("Error: Overseas airport code did not match any in database!\n")
-    time.sleep(2)
-    print("Operation complete, returning to main menu... \n")
-    time.sleep(1)
+    repeat = airportDetails()
     return repeat
   #FLIGHT DETAILS
   if choice == 2:
-    aircraftType = input("Enter type of aircraft\n")
-    print("\n")
-    if aircraftType != details[0][0] and aircraftType != details[1][0] and aircraftType != details[2][0]:
-        print("\nError: invalid aircraft type\n")
-        time.sleep(1)
-        print("Returning to main menu...\n")
-        time.sleep(1)
-        repeat = True
-        return repeat
-    elif aircraftType == details[0][0]:
-      aircraftID = 0
-      time.sleep(0.5)
-      print("Name: " + details[0][0])
-      time.sleep(0.5)
-      print("Running cost per seat per 100km: " + details[0][1])
-      time.sleep(0.5)
-      print("Maximum flight range (km): " + details[0][2])
-      time.sleep(0.5)
-      print("Maximum number of economy seats:: " + details[0][3])
-      time.sleep(0.5)
-      print("Maximum number of first-class seats: " + details[0][4])
-      print("\n")
-      time.sleep(1)
-      firstClass = int(input("Enter number of first class seats\n"))
-      if firstClass != 0:
-        if firstClass > int(details[aircraftID][4]):
-          print("Error: number of first-class seats too large")
-          repeat = True
-          return repeat
-        else:
-          print(f"Number of economy seats: {int(details[aircraftID][3]) - (firstClass*2)}")
-      print("\nOperation complete, returning to main menu... \n")
-      time.sleep(1)
-      repeat = True
-      return repeat
-    elif aircraftType == details[1][0]:
-      aircraftID = 1
-      time.sleep(0.5)
-      print("Name: " + details[1][0])
-      time.sleep(0.5)
-      print("Running cost per seat per 100km: " + details[1][1])
-      time.sleep(0.5)
-      print("Maximum fliht range (km): " + details[1][2])
-      time.sleep(0.5)
-      print("Maximum number of economy seats:" + details[1][3])
-      time.sleep(0.5)
-      print("Maximum number of first-class seats 5: " + details[1][4])
-      print("\n")
-      time.sleep(1)
-      firstClass = int(input("Enter number of first class seats\n"))
-      if firstClass != 0:
-        if firstClass > details[aircraftID][4]:
-          print("Error: number of first-class seats too large")
-          repeat = True
-          return repeat
-        else:
-          print(f"Number of economy seats: {details[aircraftID][3] - (firstClass*2)}")
-      print("\nOperation complete, returning to main menu... \n")
-      time.sleep(1)
-      repeat = True
-      return repeat
-    elif aircraftType == details[2][0]:
-      aircraftID = 2
-      time.sleep(0.5)
-      print("Name: " + details[2][0])
-      time.sleep(0.5)
-      print("Running cost per seat per 100km: " + details[2][1])
-      time.sleep(0.5)
-      print("Maximum flight range (km): " + details[2][2])
-      time.sleep(0.5)
-      print("Maximum number of economy seats: " + details[2][3])
-      time.sleep(0.5)
-      print("Maximum number of first-class seats: " + details[2][4])
-      print("\n")
-      time.sleep(1)
-      firstClass = int(input("Enter number of first class seats\n"))
-      if firstClass != 0:
-        if firstClass > details[aircraftID][4]:
-          print("Error: number of first-class seats too large")
-          repeat = True
-          return repeat
-        else:
-          print(f"Number of economy seats: {details[aircraftID][3] - (firstClass*2)}")
-      print("\nOperation complete, returning to main menu... \n")
-      time.sleep(1)
-      repeat = True
-      return repeat
+    repeat = flightDetails()
+    return repeat
   if choice == 5:
     print("Quitting...")
     quit()
